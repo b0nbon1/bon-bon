f37694b61d87e09cda94ad0f6e8c6eefefd0ba7d
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-04-11 17:22:20 +0300

    main app

---
+++ first/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <div :is="currentComponent" :swap-component="swapComponent"></div>
+  </div>
+</template>
+
+<script>
+import Welcome from './components/welcome.vue';
+import Loader from './components/loader.vue';
+import Home from './components/home.vue';
+
+export default {
+  name: 'app',
+   data() {
+     return{
+    currentComponent: 'Welcome'
+    }
+  },
+  components: {
+     'Welcome':Welcome,
+    'Loader':Loader,
+    'home':Home
+  },
+   methods: {
+    swapComponent: function(component) {
+      this.currentComponent = component;
+    }
+   }
+}
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Dokdo');
+*{
+  background-color: #145a3b;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+
+}
+</style>
